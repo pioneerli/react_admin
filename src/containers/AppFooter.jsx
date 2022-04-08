8ef2fc9b65cd8dd14135c68abcdf897153232c40
juliusjli <juliusjli@tencent.com>
2022-04-08 08:45:17 +0800

    init project

---
+++ src/containers/AppFooter.jsx
@@ -0,0 +1,14 @@
+/*
+ * @Descripttion: 
+ * @Author: julius
+ * @version: 
+ * @Date: 2022-03-29 22:30:16
+ * @LastEditors: julius
+ * @LastEditTime: 2022-03-29 22:59:27
+ */
+import React from 'react'
+import { Layout } from 'antd'
+
+const { Footer } = Layout
+
+export default () => <Footer className='footer'>React Admin &copy;2022 Created By 李多赞</Footer>
